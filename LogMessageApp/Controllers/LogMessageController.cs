f4e7b6e3840d057abbdd9a08735386268a7a0773
ajayrathi-eu-supply <ajay.rathi@eu-supply.com>
2022-11-22 02:09:18 +0100

    Step#5: Get Messages Per Log, Total number of messages and max age

--- LogMessageApp/Controllers/LogMessageController.cs
+++ LogMessageApp/Controllers/LogMessageController.cs
@@ -9,28 +9,53 @@ namespace LogMessageApp.Controllers
     public class LogMessageController : ControllerBase
     {
         private readonly ILogMessageRepository _logRepository;
-        
-        public LogMessageController(ILogMessageRepository logRepository)
+        private readonly IConfiguration _configuration;
+
+        public LogMessageController(ILogMessageRepository logRepository, IConfiguration configuration)
         {
             _logRepository = logRepository;
+            _configuration = configuration;
         }
+
         [HttpGet("GetLog/{logId}")]
         public JsonResult GetLog(int logId)
         {
             var logMessages = _logRepository.GetLogMessages(logId);
             return logMessages == null ? new JsonResult(NotFound()) : new JsonResult(Ok(logMessages));
         }
+
         [HttpPost]
         public JsonResult AddMessage(String name, int logId, String message)
         {
             var logMessage = _logRepository.AddMessage(name, logId, message);
             return new JsonResult(Ok(logMessage));
         }
+
         [HttpGet("GetAll")]
         public JsonResult GetAllLogMessages()
         {
             var messages = _logRepository.GetAllLogMessages();
             return new JsonResult(Ok(messages));
-        }       
+        }
+
+        //Get
+        [HttpGet("Get")]
+        public JsonResult Get()
+        {
+            var maxAgeInSec = _configuration["maxAgeInSec"];//Value of Expiry time of message in seconds
+            var totalMessages = _logRepository.GetAllLogMessages().Count(); // total number of messages in all logs.
+            //get messages per log
+            var messagesPerLog = _logRepository.GetAllLogMessages().GroupBy(n => n.LogId) 
+                        .Select(n => new
+                        {
+                            StoredLogs = n.Key,
+                            MessagesperLog = n.Count(),
+                            maxAgeInSec = maxAgeInSec
+                        })
+                        .OrderBy(n => n.StoredLogs);
+
+            return new JsonResult(messagesPerLog);
+
+        }
     }
 }
