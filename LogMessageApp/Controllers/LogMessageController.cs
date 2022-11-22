1335399810457a63d99d56e614c24bd14b5a9cc6
ajayrathi-eu-supply <ajay.rathi@eu-supply.com>
2022-11-22 01:44:09 +0100

    Step#3: Add WebAPI Controller and call the repository.

---
+++ LogMessageApp/Controllers/LogMessageController.cs
@@ -0,0 +1,36 @@
+﻿using LogMessageApp.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogMessageApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogMessageController : ControllerBase
+    {
+        private readonly ILogMessageRepository _logRepository;
+        
+        public LogMessageController(ILogMessageRepository logRepository)
+        {
+            _logRepository = logRepository;
+        }
+        [HttpGet]
+        public JsonResult GetLog(int logId)
+        {
+            var logMessages = _logRepository.GetLogMessages(logId);
+            return logMessages == null ? new JsonResult(NotFound()) : new JsonResult(Ok(logMessages));
+        }
+        [HttpPost]
+        public JsonResult AddMessage(String name, int logId, String message)
+        {
+            var logMessage = _logRepository.AddMessage(name, logId, message);
+            return new JsonResult(Ok(logMessage));
+        }
+        [HttpGet]
+        public JsonResult GetAllLogMessages()
+        {
+            var messages = _logRepository.GetAllLogMessages();
+            return new JsonResult(Ok(messages));
+        }       
+    }
+}
