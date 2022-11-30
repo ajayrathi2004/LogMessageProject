2056c79e5f3204efb981241e3a5c98ee7a9541df
ajayrathi-eu-supply <ajay.rathi@eu-supply.com>
2022-11-30 19:51:40 +0100

    Implement MOQ, AutoFixture and fluentAssertion for unit test case

--- LogMessageTest/Controller/LogMessageControllerTest.cs
+++ LogMessageTest/Controller/LogMessageControllerTest.cs
@@ -3,45 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FakeItEasy;
+using LogMessageApp.Data;
 using LogMessageApp.Repositories;
+using Microsoft.Extensions.Configuration;
 using Xunit;
+using Moq;
+using AutoFixture;
+using FluentAssertions;
+using LogMessageApp.Controllers;
+using LogMessageApp.Models;
 
 namespace LogMessageTest.Controller
 {
     public class LogMessageControllerTest
     {
-        private readonly ILogMessageRepository _logRepository;
+        private readonly IFixture _fixture;
+        private readonly Mock<ILogMessageRepository> _logRepository;
+        private readonly Mock<ApiContext> _context;
+        private readonly IConfiguration _configuration;
+        private readonly LogMessageController _logMessageController;
 
         public LogMessageControllerTest()
         {
-            _logRepository = A.Fake<ILogMessageRepository>();
+            //_logRepository = A.Fake<ILogMessageRepository>();
+            //_context = A.Fake<ApiContext>();
+            //_configuration = A.Fake<IConfiguration>();
+
+            _fixture=new Fixture();
+            _context = _fixture.Freeze<Mock<ApiContext>>();
+            _logRepository = _fixture.Freeze<Mock<ILogMessageRepository>>();
+            //_configuration = _fixture.Freeze<Mock<IConfiguration>>();
+            _logMessageController = new LogMessageController(_logRepository.Object, _configuration);
+
         }
         [Fact]
         public void LogMessagesControllerTest_GetLogMessage_ResponseOK()
         {
             //Arrange
-            int logId = 1;
+            var logReturnMock = _fixture.Create<List<LogMessage>>();
+            var logId = 1;
+            _logRepository.Setup(x=>x.GetLogMessages(logId)).Returns(logReturnMock);
 
-            // Act on Test  
-            var response = _logRepository.GetLogMessages(logId);
+            //Act
+            var results = _logMessageController.GetLog(logId);
 
             // Assert the result  
-            Assert.NotNull(response);
+            results.Should().NotBeNull();
+         
+
+            // Below is the use of FakeItEasy
+
+            ////Arrange
+            //int logId = 1;
+
+            //var caller = new LogMessageRepository(_context, _configuration);
+            //// Act on Test  
+            //var response = caller.GetLogMessages(logId);
+
+            //// Assert the result  
+            //Assert.NotNull(response);
         }
         [Fact]
         public void LogMessagesControllerTest_AddLogMessage_ResponseOK()
         {
-            //Arrange
-            int logId = 1;
-            string name = "Suplier";
-            string message = "hello, How are you?";
+            ////Arrange
+            //int logId = 1;
+            //string name = "Suplier";
+            //string message = "hello, How are you?";
 
-            // Act on Test  
-            var response = _logRepository.AddMessage(name, logId, message);
+            //var caller = new LogMessageRepository(_context, _configuration);
+            //// Act on Test  
+            //var response = caller.AddMessage(name, logId, message);
 
-            // Assert the result  
-            Assert.NotNull(response);
+            //// Assert the result  
+            //Assert.NotNull(response);
         }
     }
 }
